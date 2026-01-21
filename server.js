cd09ec3134a0bab97d413e8ea299b427678d6451
todaytheofficial <todaytheofficial@gmail.com>
2026-01-21 13:54:32 +0600

    Update server.js

--- server.js
+++ server.js
@@ -469,7 +469,7 @@ app.get('/api/stats', async (req, res) => {
 });
 
 // Serve index.html for all other routes
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
